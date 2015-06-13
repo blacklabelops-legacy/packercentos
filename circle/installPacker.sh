5eac4490612036dcdf2123420a3868238c306257
Steffen Bleul <blacklabelops@itbleul.de>
2015-06-13 10:38:10 +0200

    Ci script corrections.

--- circle/installPacker.sh
+++ circle/installPacker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -e ${HOME}/packer ]
+if [ ! -e ${HOME}/bin/packer ]
 then
   wget -O /tmp/packer.zip https://dl.bintray.com/mitchellh/packer/packer_0.7.5_linux_amd64.zip
   unzip -od ${HOME}/bin /tmp/packer.zip
