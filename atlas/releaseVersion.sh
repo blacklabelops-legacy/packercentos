ca52381ed6d7799cd1b809513f09d55ba852a34a
Steffen Bleul <blacklabelops@itbleul.de>
2015-06-13 14:18:48 +0200

    Script for releasing an atlas artifact.

---
+++ atlas/releaseVersion.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -o errexit    # abort script at first error
+
+# Setting environment variables
+readonly CUR_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+
+# Reading config
+source $CUR_DIR/atlas.cfg
+
+#Checks for an Atlas API Error
+function checkErr {
+	if  [[ $1 == '{"errors":'* ]] ;
+	then
+		echo $1
+    	err
+	fi
+}
+
+# Helper functions
+err() {
+  printf '%b\n' ""
+  printf '%b\n' "\033[1;31m[ATLAS ERROR] $@\033[0m"
+  printf '%b\n' ""
+  exit 1
+} >&2
+
+#------------------
+# SCRIPT ENTRYPOINT
+#------------------
+
+response=$(curl https://atlas.hashicorp.com/api/v1/box/${ATLAS_BOX}/version/${ATLAS_VERSION}/release  -X PUT -d access_token=${ATLAS_TOKEN})
+
+checkErr $response
+
+echo $response
