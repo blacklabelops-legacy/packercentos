557c48b48f5c3f29b91f6f16ecf7378fc8ba5ee5
Steffen Bleul <blacklabelops@itbleul.de>
2015-06-13 10:35:17 +0200

    Do not download packer when circle cache exists.

---
+++ circle/installPacker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ ! -e ${HOME}/packer ]
+then
+  wget -O /tmp/packer.zip https://dl.bintray.com/mitchellh/packer/packer_0.7.5_linux_amd64.zip
+  unzip -od ${HOME}/bin /tmp/packer.zip
+fi
+
+packer version
