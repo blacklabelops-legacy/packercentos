4d8fc669183e442ce292280d81321f192600cda7
Steffen Bleul <blacklabelops@itbleul.de>
2015-06-14 10:22:13 +0200

    Fixed version parameters in provider scripts.

--- atlas/createProvider.sh
+++ atlas/createProvider.sh
@@ -10,7 +10,7 @@ source $CUR_DIR/atlas.cfg
 function checkErr {
 	if  [[ $1 == '{"errors":'* ]] ;
 	then
-		echo $1
+		echo $response
     	err
 	fi
 }
@@ -29,9 +29,8 @@ err() {
 
 version=$1
 
-if [ version == 0 ]; then
+if [ -z "$1" ]; then
   version=${ATLAS_VERSION}
-  exit 1
 fi
 
 response=$(curl -s https://atlas.hashicorp.com/api/v1/box/${ATLAS_BOX}/version/${version}/providers -X POST -d provider[name]=${ATLAS_PROVIDER} -d access_token=${ATLAS_TOKEN})
--- atlas/deleteProvider.sh
+++ atlas/deleteProvider.sh
@@ -6,4 +6,14 @@ readonly CUR_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 # Reading config
 source $CUR_DIR/atlas.cfg
 
-curl https://atlas.hashicorp.com/api/v1/box/${ATLAS_BOX}/version/${ATLAS_VERSION}/provider/${ATLAS_PROVIDER} -X DELETE -d access_token=${ATLAS_TOKEN}
+#------------------
+# SCRIPT ENTRYPOINT
+#------------------
+
+version=$1
+
+if [ -z "$1" ]; then
+  version=${ATLAS_VERSION}
+fi
+
+curl https://atlas.hashicorp.com/api/v1/box/${ATLAS_BOX}/version/${version}/provider/${ATLAS_PROVIDER} -X DELETE -d access_token=${ATLAS_TOKEN}
