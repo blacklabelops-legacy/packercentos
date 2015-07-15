b7e6e4928f6f14ec2dd457c2d81023c1d3e9ca0a
Steffen Bleul <sbl@itbleul.de>
2015-07-15 18:08:28 +0200

    Updgraded Packer Version.

--- circle/installPacker.sh
+++ circle/installPacker.sh
@@ -4,7 +4,7 @@ set -o errexit    # abort script at first error
 
 if [ ! -e ${HOME}/bin/packer ]
 then
-  wget -O /tmp/packer.zip https://dl.bintray.com/mitchellh/packer/packer_0.7.5_linux_amd64.zip
+  wget -O /tmp/packer.zip https://dl.bintray.com/mitchellh/packer/packer_0.8.1_linux_amd64.zip
   unzip -od ${HOME}/bin /tmp/packer.zip
 fi
 
