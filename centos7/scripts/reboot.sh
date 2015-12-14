4a016b34e8d8f012de67052b852eb782488584f1
Steffen Bleul <blacklabelops@itbleul.de>
2015-12-14 18:56:19 +0100

    Removed subproject which moved to blacklabelops/baseboxes.

--- centos7/scripts/reboot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#Implementing reboot for problem with kernel updates
-reboot
-sleep 60
--- centos7/scripts/sshd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "UseDNS no" >> /etc/ssh/sshd_config
-echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
