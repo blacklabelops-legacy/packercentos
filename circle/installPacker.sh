e4ad8c41e396d7107fb8fa3b25de53a9793722bf
Steffen Bleul <blacklabelops@itbleul.de>
2016-03-23 17:21:11 +0100

    Fixed download location.

--- circle/installPacker.sh
+++ circle/installPacker.sh
@@ -4,7 +4,7 @@ set -o errexit    # abort script at first error
 
 if [ ! -e ${HOME}/bin/packer ]
 then
-  wget -O /tmp/packer.zip https://dl.bintray.com/mitchellh/packer/packer_0.8.1_linux_amd64.zip
+  wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/0.10.0/packer_0.10.0_linux_amd64.zip
   unzip -od ${HOME}/bin /tmp/packer.zip
 fi
 
